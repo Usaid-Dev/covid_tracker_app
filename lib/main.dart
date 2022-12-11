74809f714157e843974a95b91a8aed2ecd28247d
Usaid Asif <usaidasif12@gmail.com>
2022-12-11 14:22:18 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:covid_tracker_app/View/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
