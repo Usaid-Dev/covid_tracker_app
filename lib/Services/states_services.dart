d386b1170a10ef2420e93b5f87c34edf344fd408
Usaid Asif <usaidasif12@gmail.com>
2022-12-15 13:55:00 +0500

    Fetched countries list api & added app icon

--- lib/Services/states_services.dart
+++ lib/Services/states_services.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:covid_tracker_app/Model/world_states_model.dart';
 import 'package:covid_tracker_app/Services/Utilities/app_url.dart';
-import 'package:covid_tracker_app/View/world_states.dart';
 import 'package:http/http.dart' as http;
 
 class StatesServices {
@@ -11,6 +10,17 @@ class StatesServices {
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
       return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+  Future<List<dynamic>> CountriesListApi() async {
+    var data;
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      return data;
     } else {
       throw Exception('Error');
     }
--- lib/View/countries_list.dart
+++ lib/View/countries_list.dart
@@ -1,4 +1,6 @@
+import 'package:covid_tracker_app/Services/states_services.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CountriesList extends StatefulWidget {
   const CountriesList({super.key});
@@ -8,8 +10,131 @@ class CountriesList extends StatefulWidget {
 }
 
 class _CountriesListState extends State<CountriesList> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    StatesServices statesServices = StatesServices();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: searchController,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                  hintText: 'Search with country name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: statesServices.CountriesListApi(),
+                builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                  if (!snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: 15,
+                      itemBuilder: ((context, index) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.grey.shade700,
+                          highlightColor: Colors.grey.shade100,
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: Container(
+                                  height: 10,
+                                  width: 80,
+                                  color: Colors.white,
+                                ),
+                                subtitle: Container(
+                                  height: 10,
+                                  width: 80,
+                                  color: Colors.white,
+                                ),
+                                leading: Container(
+                                  height: 50,
+                                  width: 50,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      }),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: ((context, index) {
+                        String name = snapshot.data![index]['country'];
+                        if (searchController.text.isEmpty) {
+                          return Column(
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  snapshot.data![index]['country'],
+                                ),
+                                subtitle: Text(
+                                  snapshot.data![index]['cases'].toString(),
+                                ),
+                                leading: Image(
+                                  height: 50,
+                                  width: 50,
+                                  image: NetworkImage(
+                                    snapshot.data![index]['countryInfo']
+                                        ['flag'],
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        } else if (name
+                            .toLowerCase()
+                            .contains(searchController.text.toLowerCase())) {
+                          return Column(
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  snapshot.data![index]['country'],
+                                ),
+                                subtitle: Text(
+                                  snapshot.data![index]['cases'].toString(),
+                                ),
+                                leading: Image(
+                                  height: 50,
+                                  width: 50,
+                                  image: NetworkImage(
+                                    snapshot.data![index]['countryInfo']
+                                        ['flag'],
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
